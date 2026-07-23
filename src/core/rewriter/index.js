31655aecee027af6491d4d27c66c886be1f621a3
pokiprohajj <pokiprohajj@users.noreply.github.com>
2026-07-23 15:28:42 +0100

    add TikTok pixel

--- src/core/rewriter/index.js
+++ src/core/rewriter/index.js
@@ -126,7 +126,14 @@ function rewriteHtml(html, sourcePath) {
   // Pass 9b: Rewrite games.poki.com URLs in INITIAL_STATE server-side (most reliable)
   rewriteGameInitState($, sourcePath);
 
-  // Pass 9c: On ALL pages, inject iframe src interceptor (persists across SPA navigations)
+  // Pass 9c: On ALL pages, inject TikTok pixel
+  if ($('head').length) {
+    $('head').append('<script>' +
+      '!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};ttq.load("D9H295BC77UA1IJTIG30");ttq.page()}(window,document,"ttq");' +
+    '</script>');
+  }
+
+  // Pass 9d: On ALL pages, inject iframe src interceptor (persists across SPA navigations)
   if ($('head').length) {
     $('head').append('<script id="portal-iframe-rw">' +
       '(function(){' +
